1fd70ee721c95d9ff570f63fc545712260e6922a
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2025-06-21 20:52:00 +0200

    feat: teleport architect if player goes to configured world

--- src/main/java/io/github/milkdrinkers/stewards/listener/PlayerListener.java
+++ src/main/java/io/github/milkdrinkers/stewards/listener/PlayerListener.java
@@ -6,6 +6,9 @@ import io.github.milkdrinkers.stewards.steward.StewardLookup;
 import io.github.milkdrinkers.stewards.towny.TownMetaData;
 import io.github.milkdrinkers.stewards.trait.ArchitectTrait;
 import io.github.milkdrinkers.stewards.trait.StewardTrait;
+import io.github.milkdrinkers.stewards.utility.Cfg;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -49,6 +52,31 @@ public class PlayerListener implements Listener {
             trait.setFollowing(false);
             trait.setFollowingPlayer(null);
         }
+    }
+
+    @EventHandler
+    public void onPlayerTeleportToSpawn(PlayerTeleportEvent e) {
+        // Return if player is followed, handled above.
+        if (StewardLookup.get().isPlayerFollowed(e.getPlayer())) return;
+
+        if (!Cfg.get().getBoolean("architect.teleport.enabled")) return;
+
+        // Get the configured world and null check
+        World world = Bukkit.getWorld(Cfg.get().getString("architect.teleport.world"));
+        if (world == null) return;
+
+        if (e.getTo().getWorld() != world) return;
+
+        Steward steward = StewardLookup.get().getSteward(TownyAPI.getInstance().getTown(e.getPlayer()).getMayor().getUUID());
+        if (steward == null) return;
+
+        // If steward is not an architect, don't continue
+        if (!steward.getSettler().getNpc().hasTrait(ArchitectTrait.class)) return;
+
+        // If architect is hired, i.e. town is created, don't continue
+        if (steward.getSettler().getNpc().getOrAddTrait(StewardTrait.class).isHired()) return;
+
+        steward.getSettler().getNpc().teleport(e.getTo(), PlayerTeleportEvent.TeleportCause.PLUGIN);
     }
 
 }
